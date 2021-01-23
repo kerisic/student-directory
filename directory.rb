dc8e023b8ac45875fd36e5f71eddae8f0f159288
Christine Chee <christinechee88@hotmail.com>
2021-01-23 20:41:19 +0000

    Add save and load list methods

--- directory.rb
+++ directory.rb
@@ -13,7 +13,7 @@ def input_students
     hobby = gets.chomp
     puts "What is the student's country of birth?"
     country = gets.chomp
-    @students << {name: name, cohort: cohort, hobby: hobby, country: country}
+    @students << {name: name, cohort: cohort.to_sym, hobby: hobby, country: country}
     if @students.length == 1
       puts "Now we have 1 student"
     else
@@ -52,6 +52,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"  
 end
 
@@ -73,6 +74,15 @@ def save_students
   file.close
 end
 
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+  name, cohort = line.chomp.split(" , ")
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+end
+
 def process(selection)
   case selection
     when "1"
@@ -81,6 +91,8 @@ def process(selection)
       show_students
     when "3"
       save_students
+    when "4"
+      load_students
     when "9"
       exit#this will cause the program to terminate
     else
