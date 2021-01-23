eb62caa44c2936d72f32651ee8121e729d62da03
Christine Chee <christinechee88@hotmail.com>
2021-01-23 18:27:10 +0000

    Add interactive menu

--- directory.rb
+++ directory.rb
@@ -1,36 +1,85 @@
+@students = [] # an empty array accessible to all methods
 #Let's put all students into an array
 def input_students
   puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
-  #create an empty array
-  students = []
+  puts "To finish, just hit return"
+  @students = []
+  puts "What is the student's name?"
   name = gets.chomp
   while !name.empty? do
-    # add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    puts "Which cohort?"
+    cohort = gets.chomp
+    cohort = :november if cohort.empty? == true
+    puts "What is the student's hobby?"
+    hobby = gets.chomp
+    puts "What is the student's country of birth?"
+    country = gets.chomp
+    @students << {name: name, cohort: cohort, hobby: hobby, country: country}
+    if @students.length == 1
+      puts "Now we have 1 student"
+    else
+      puts "Now we have #{@students.count} students"
+    end
     #get another name from the user
+    puts "What is the student's name?"
     name = gets.chomp
+    if name.empty? == true
+      break
+    end
   end
-  #return the array of students
-  students
 end
 
 def print_header
-puts "The students of Villains Academy"
-puts "-------------"
-end
-def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
-  end
-end
-def print_footer(students)
- puts "Overall, we have #{students.count} great students"
+puts "The students of Villains Academy".center(70)
+puts "-------------".center(70)
 end
 
-students = input_students
-#nothing happens until we call the methods
-print_header
-print(students)
-print_footer(students)
+def print_students_list
+  index = 0
+  while true do    
+    puts "#{index + 1}. #{@students[index][:name]}, Hobby: #{@students[index][:hobby]}, #{@students[index][:country]} (#{@students[index][:cohort]} cohort)".center(70)
+    index += 1
+    if index >= @students.length
+      break
+    end
+  end
+end
+
+def print_footer
+  puts "Overall, we have #{@students.count} great students".center(70)
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"  
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit#this will cause the program to terminate
+    else
+      puts "I don't know what you meant, try again"
+  end
+end
+
+def interactive_menu
+  @students = []
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+interactive_menu
