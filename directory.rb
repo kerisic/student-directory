52bb139fc0d18a012be55a45ae29cd86c7911c8f
Christine Chee <christinechee88@hotmail.com>
2021-01-23 21:26:29 +0000

    Complete student directory app

--- directory.rb
+++ directory.rb
@@ -1,53 +1,5 @@
 @students = [] # an empty array accessible to all methods
 
-def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return" 
-  puts "What is the student's name?"
-  name = gets.chomp
-  while !name.empty? do
-    puts "Which cohort?"
-    cohort = gets.chomp
-    cohort = :november if cohort.empty? == true
-    puts "What is the student's hobby?"
-    hobby = gets.chomp
-    puts "What is the student's country of birth?"
-    country = gets.chomp
-    @students << {name: name, cohort: cohort.to_sym, hobby: hobby, country: country}
-    if @students.length == 1
-      puts "Now we have 1 student"
-    else
-      puts "Now we have #{@students.count} students"
-    end
-    #get another name from the user
-    puts "What is the student's name?"
-    name = gets.chomp
-    if name.empty? == true
-      break
-    end
-  end
-end
-
-def print_header
-puts "The students of Villains Academy".center(70)
-puts "-------------".center(70)
-end
-
-def print_students_list
-  index = 0
-  while true do    
-    puts "#{index + 1}. #{@students[index][:name]}, Hobby: #{@students[index][:hobby]}, #{@students[index][:country]} (#{@students[index][:cohort]} cohort)".center(70)
-    index += 1
-    if index >= @students.length
-      break
-    end
-  end
-end
-
-def print_footer
-  puts "Overall, we have #{@students.count} great students".center(70)
-end
-
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
@@ -56,31 +8,11 @@ def print_menu
   puts "9. Exit"  
 end
 
-def show_students
-  print_header
-  print_students_list
-  print_footer
-end
-
-def save_students
-  # open the file for writing
-  file = File.open("students.csv", "w")
-  #iterate over the array of students
-  @students.each do |student|
-    student_data = [student[:name], student[:cohort]]
-    csv_line = student_data.join(" , ")
-    file.puts csv_line
+def interactive_menu
+  loop do
+    print_menu
+    process(STDIN.gets.chomp)
   end
-  file.close
-end
-
-def load_students
-  file = File.open("students.csv", "r")
-  file.readlines.each do |line|
-  name, cohort = line.chomp.split(" , ")
-    @students << {name: name, cohort: cohort.to_sym}
-  end
-  file.close
 end
 
 def process(selection)
@@ -100,12 +32,83 @@ def process(selection)
   end
 end
 
-def interactive_menu
-  @students = []
-  loop do
-    print_menu
-    process(gets.chomp)
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return" 
+  puts "What is the student's name?"
+  name = STDIN.gets.chomp
+  while !name.empty? do
+    puts "Which cohort?"
+    cohort = STDIN.gets.chomp
+    cohort = november if cohort.empty? == true
+    @students << {name: name, cohort: cohort.to_sym}
+    if @students.length == 1
+      puts "Now we have 1 student"
+    else
+      puts "Now we have #{@students.count} students"
+    end
+    #get another name from the user
+    puts "What is the student's name?"
+    name = STDIN.gets.chomp
+    if name.empty? == true
+      break
+    end
   end
 end
 
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def print_header
+puts "The students of Villains Academy".center(70)
+puts "-------------".center(70)
+end
+
+def print_students_list
+  @students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer
+  puts "Overall, we have #{@students.count} great students".center(70)
+end
+
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  #iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(" , ")
+    file.puts csv_line
+  end
+  file.close
+end
+
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
+  file.readlines.each do |line|
+  name, cohort = line.chomp.split(" , ")
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+end
+
+def try_load_students
+  filename = ARGV.first
+  return if filename.nil? 
+  if File.exists?(filename)
+    load_students(filename)
+      puts "Loaded #{@students.count} from #{filename}"
+  else
+    puts "Sorry, #{filename} doesn't exist."
+    exit
+  end
+end
+
+try_load_students
 interactive_menu
